656eddaf44168a1ba58bcb57db114e51c439fc23
danielcajusol <gerson.cajusol@mi.unc.edu.ar>
2026-06-18 18:12:13 -0300

    fix: dashboard metrics

--- frontend/dashboard/src/components/dashboard/DashboardContainer.tsx
+++ frontend/dashboard/src/components/dashboard/DashboardContainer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, SimpleGrid, Heading, Stat, Text} from "@chakra-ui/react";
+import { Box, SimpleGrid, Heading, Stat, Text } from "@chakra-ui/react";
 import { HumidityChart } from "@/components/charts/HumidityChart";
 import { sensorType } from "@/types/main.types";
 import { useLatestMetrics } from "@/hooks/useLatestMetrics";
@@ -52,7 +52,7 @@ export function DashboardContainer() {
                 {type[card.sensorType]}
               </Stat.Label>
               <Stat.ValueText fontSize="2xl" fontWeight="bold">
-                {card.value} {card.sensorType == "temp" ? "°C" : "%"}
+                {card.value.toFixed(2)} {card.sensorType == "temp" ? "°C" : "%"}
               </Stat.ValueText>
               <Stat.HelpText color="gray.400" fontSize="xs" mb={0}>
                 ESP32-INV-01
